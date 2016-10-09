26f5772d6be4a57dbc788b72792ab9619b117cf9
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2016-10-09 13:11:20 +0200

    SymbolType, SymbolDefinition and SymbolReference for Cobol 2002 user-defined data types

--- TypeCobol/Compiler/Diagnostics/Cobol85Checkers.cs
+++ TypeCobol/Compiler/Diagnostics/Cobol85Checkers.cs
@@ -423,7 +423,7 @@ class WriteTypeConsistencyChecker: NodeListener {
 				var node = (DataDescription)GetSymbol(table, qname);
 				entry = node.CodeElement();
 			} else throw new NotImplementedException(data.CodeElement.GetType().Name);
-			if (entry.CustomType == null) return entry.DataType;//not a custom type
+			if (entry.UserDefinedDataType == null) return entry.DataType;//not a custom type
 		}
         ITypedNode typed = symbol as ITypedNode;
 		if (typed == null) return null;// symbol untyped
